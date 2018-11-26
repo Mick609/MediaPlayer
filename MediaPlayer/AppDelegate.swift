d682cec2b8092074dd92f830fd39e0a014c30cb5
mick <mick609@outlook.com>
2018-11-26 13:04:03 +0800

    Initial Commit

---
+++ MediaPlayer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MediaPlayer
+//
+//  Created by Mick Shi on 19/11/18.
+//  Copyright © 2018 Mick Shi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
